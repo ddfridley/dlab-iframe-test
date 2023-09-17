9a2135ef9a2c83da348e6096905e407b6d88f0ac
David Fridley <ddfridley@yahoo.com>
2023-09-17 15:31:54 -0700

    C:/Program Files/Git/dev and prod-mirror

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 let server = express();
 let port = process.env.PORT || 3333;
 
+server.get('/dev', (request, response) => {
+    // __dirname is the absolute path of the currently executing file, index.js
+    // if you don't specify a root option then use an absolute path for the html file
+    response.sendFile('./public/dev.html', { root: __dirname });
+})
 server.get('/', (request, response) => {
     // __dirname is the absolute path of the currently executing file, index.js
     // if you don't specify a root option then use an absolute path for the html file
